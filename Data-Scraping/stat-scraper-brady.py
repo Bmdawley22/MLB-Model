29686e13881315d682be81ee7d838e2303078558
Bmdawley22 <dawleyb11@gmail.com>
2025-06-13 10:56:34 -0500

    Forgot to uncomment URLS

--- Data-Scraping/stat-scraper-brady.py
+++ Data-Scraping/stat-scraper-brady.py
@@ -16,16 +16,16 @@ STAT_URLS = [
         "sheet_name": "(B) Pitch Val / 100",
         "parent_div_class_target": "table-scroll"
     },
-    # {
-    #     "url": "https://www.fangraphs.com/leaders/major-league?stats=pit&lg=all&type=9&season=2025&season1=2025&ind=0&rost=&age=&filter=&players=0&team=0&pageitems=2000000000&pos=all&qual=10&month=0",
-    #     "sheet_name": "(P) Pitch Splits",
-    #     "parent_div_class_target": "table-scroll"
-    # },
-    # {
-    #     "url": "https://www.fangraphs.com/leaders/major-league?stats=pit&lg=all&type=13&season=2025&season1=2025&ind=0&rost=&age=&filter=&players=0&team=0&pageitems=2000000000&pos=all&qual=10&month=0",
-    #     "sheet_name": "(P) Pitch Val / 100",
-    #     "parent_div_class_target": "table-scroll"
-    # },
+    {
+        "url": "https://www.fangraphs.com/leaders/major-league?stats=pit&lg=all&type=9&season=2025&season1=2025&ind=0&rost=&age=&filter=&players=0&team=0&pageitems=2000000000&pos=all&qual=10&month=0",
+        "sheet_name": "(P) Pitch Splits",
+        "parent_div_class_target": "table-scroll"
+    },
+    {
+        "url": "https://www.fangraphs.com/leaders/major-league?stats=pit&lg=all&type=13&season=2025&season1=2025&ind=0&rost=&age=&filter=&players=0&team=0&pageitems=2000000000&pos=all&qual=10&month=0",
+        "sheet_name": "(P) Pitch Val / 100",
+        "parent_div_class_target": "table-scroll"
+    },
 
     # Add more URL and sheet name pairs as needed
 ]
